b46f3d0f085ceb8f055729fdff71f9f2cda71954
Harjot Singh <harjot@harkul.com>
2018-02-06 10:22:37 +0000

    lab2: basic boilerplate

---
+++ src/lab2/Behaviours/MoveForward.java
@@ -0,0 +1,20 @@
+package lab2.Behaviours;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class MoveForward implements Behavior {
+    @Override
+    public boolean takeControl() {
+        return true;
+    }
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public void suppress() {
+
+    }
+}
---
+++ src/lab2/Robot.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class Robot {
+
+
+    public static void main(String args[]) {
+
+    }
+}
