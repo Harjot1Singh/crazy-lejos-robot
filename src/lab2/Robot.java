66906c9eebf958f7f6e7aed65f40740ffb3bf174
Harjot Singh <harjot@harkul.com>
2018-02-06 10:35:03 +0000

    lab2: use behaviours and start arb

--- src/lab2/Robot.java
+++ src/lab2/Robot.java
@@ -1,9 +1,26 @@
 package lab2;
 
+import lejos.robotics.subsumption.Arbitrator;
+
+import lab2.Behaviours.MoveForward;
+import lejos.robotics.subsumption.Behavior;
+
+import java.util.ArrayList;
+
 public class Robot {
 
+    private static Behavior[] setupBehaviours() {
+        ArrayList<Behavior> behaviours = new ArrayList<>();
+
+        // Add Behaviours
+        behaviours.add(new MoveForward());
+
+        return behaviours.toArray(new Behavior[behaviours.size()]);
+    }
 
     public static void main(String args[]) {
+        Arbitrator arbitrator = new Arbitrator(setupBehaviours());
 
+        arbitrator.start();
     }
 }
