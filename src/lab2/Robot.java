40ed433968936cc7c1179d8c0224e28fd427dab9
Harjot Singh <harjot@harkul.com>
2018-02-12 20:20:29 +0000

    lab2: simplify behaviours

--- src/lab2/Robot.java
+++ src/lab2/Robot.java
@@ -1,24 +1,18 @@
 package lab2;
 
-import java.util.ArrayList;
 import lejos.robotics.subsumption.Behavior;
 import lejos.robotics.subsumption.Arbitrator;
 
 import lab2.Behaviours.MoveForward;
 
 public class Robot {
+    private static Behavior[] behaviors = {
+            new MoveForward()
+    };
 
-    private static Behavior[] setupBehaviours() {
-        ArrayList<Behavior> behaviours = new ArrayList<>();
-
-        // Add Behaviours
-        behaviours.add(new MoveForward());
-
-        return behaviours.toArray(new Behavior[behaviours.size()]);
-    }
 
     public static void main(String args[]) {
-        Arbitrator arbitrator = new Arbitrator(setupBehaviours());
+        Arbitrator arbitrator = new Arbitrator(behaviors);
 
         arbitrator.start();
     }
