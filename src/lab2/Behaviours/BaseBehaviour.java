2df1746eaf10f2e49b30a0a39f7769cd28666a3e
Harjot Singh <harjot@harkul.com>
2018-02-13 00:24:01 +0000

    lab2: add rotation and stopping to `BaseBehaviour`

--- src/lab2/Behaviours/BaseBehaviour.java
+++ src/lab2/Behaviours/BaseBehaviour.java
@@ -30,7 +30,35 @@ public abstract class BaseBehaviour implements Behavior {
         supressed = false;
     }
 
-    protected boolean isOverLine(LightSensor sensor) {
+    /**
+     * Returns true if the sensor is on top of a line.
+     * @param sensor The sensor to check.
+     * @return True if on top of a line.
+     */
+    boolean isOverLine(LightSensor sensor) {
         return sensor.readValue() < lineThreshold;
+    }
+
+
+    /**
+     * Rotates the robot on the spot.
+     * @param turnRight If true, rotates right, else left.
+     */
+    void rotate(boolean turnRight) {
+        if (turnRight) {
+            rightMotor.backward();
+            leftMotor.forward();
+        } else {
+            rightMotor.forward();
+            leftMotor.backward();
+        }
+    }
+
+    /**
+     * Immediately stops any motor movement.
+     */
+    void stop() {
+        leftMotor.stop(true);
+        rightMotor.stop(true);
     }
 }
