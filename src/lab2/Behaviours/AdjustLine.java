940c972fb5f0939029ed2c4a007bcc548f120a79
Harjot Singh <harjot@harkul.com>
2018-02-13 00:27:51 +0000

    lab2: implement adjust line behaviour

--- src/lab2/Behaviours/AdjustLine.java
+++ src/lab2/Behaviours/AdjustLine.java
@@ -1,22 +1,35 @@
 package lab2.Behaviours;
 
+import lejos.nxt.LightSensor;
+
+/**
+ * Behaviour to adjust the robot to stay on the line.
+ * The robot is defined as being displaced if either light sensor is over the line.
+ * The robot should rotate in the opposite direction until the light sensor is no longer over the line.
+ */
 public class AdjustLine extends BaseBehaviour {
 
     @Override
     public boolean takeControl() {
-        return true;
+        return isOverLine(leftSensor) || isOverLine(rightSensor);
     }
 
     @Override
     public void action() {
-        // While more than threshold, keep turning
+        boolean turnRight = isOverLine(rightSensor);
+        LightSensor sensor = turnRight ? rightSensor : leftSensor;
 
-        while (!isSupressed()) {
+        // Start rotating in the correctional direction
+        rotate(turnRight);
+
+        // Until the sensor is no longer over the line
+        while (!isSupressed() && isOverLine(sensor)) {
             Thread.yield();
         }
 
+        stop();
         // If 360 has been done, the line has ended??
-        System.exit(0);
+        // System.exit(0);
     }
 }
 
