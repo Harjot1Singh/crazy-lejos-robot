a7cceb31eb37aa89537ecbe9139cf621735051c1
Harjot Singh <harjot@harkul.com>
2018-02-27 09:37:52 +0000

    object detection

--- src/lab4/Behaviours/AvoidObject.java
+++ src/lab4/Behaviours/AvoidObject.java
@@ -1,5 +1,7 @@
 package lab4.Behaviours;
 
+import lejos.nxt.LCD;
+
 public class AvoidObject extends BaseBehaviour {
 	private final static int threshold = 800;
 
@@ -12,6 +14,8 @@ public class AvoidObject extends BaseBehaviour {
 	@Override
 	public void action() {
 		super.action();
+		LCD.drawString("AvoidObject", 0, 0);
+
 		pilot.rotate(180);
 	}
 }
